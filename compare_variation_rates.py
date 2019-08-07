2db7445210cc9f69d006ae491924d7c2f65741d3
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2019-08-07 12:21:16 +0200

    compare variant positions to splice site positions

--- compare_variation_rates.py
+++ compare_variation_rates.py
@@ -6,104 +6,117 @@ __usage__ = """
 					python compare_variation_rates.py
 					--vcf <FULL_PATH_TO_VCF_FILE>
 					--ncss <FULL_PATH_TO_SPLICE_SITE_FILE>
+					--gff <FULL_PATH_TO_ANNOTATION_FILE>
+					--fasta <FULL_PATH_TO_GENOME_FILE>
 					"""
 
-from scipy import stats
-import sys
+import sys, re
 
 # --- end of imports --- #
 
-
-def load_ncss( ncss_file ):
-	"""! @brief load all minor ncss """
+def load_chr_per_gene( gff_file ):
+	"""! @brief load chromosomes of all genes """
 	
-	minor_ncss = []
-	total_splice_site_length = 0
+	chr_per_gene = {}
+	with open( gff_file, "r" ) as f:
+		line = f.readline()
+		while line:
+			if line[0] != '#':
+				parts = line.strip().split('\t')
+				if parts[2] == "mRNA":
+					try:
+						ID = re.findall( "rna\d+", parts[-1] )[0]
+						try:
+							chr_per_gene[ ID ]
+						except KeyError:
+							chr_per_gene.update( { ID: parts[0] } )
+					except IndexError:
+						print line
+			line = f.readline()
+	return chr_per_gene
+
+
+def load_splice_site_pos( ncss_file, chr_per_gene ):
+	"""! @brief load all splice site positions """
+	
+	css_splice_site_pos = {}
+	major_ncss_splice_site_pos = {}
+	minor_ncss_splice_site_pos = {}
+	
+	css_total_len = 0
+	major_ncss_total_len = 0
+	minor_ncss_total_len = 0
+	
 	with open( ncss_file, "r" ) as f:
 		line = f.readline()
 		while line:
 			parts = line.strip().split('\t')
 			if len( parts ) > 3:
-				total_splice_site_length += 4
-				if parts[3] == "ncss":
-					if parts[1] == "GC" and parts[2] == "AG":
-						pass
-					elif parts[1] == "AT" and parts[2] == "AC":
-						pass
-					else:
-						minor_ncss.append( parts[1]+parts[2] )
+				
+				subparts = parts[4].split(',')
+				chromosome = chr_per_gene[ parts[0] ]
+				if parts[1:3] == [ "GT", "AG" ]:
+					css_total_len += 4
+					for each in subparts:
+						start = int( each.split('_')[0] )
+						css_splice_site_pos.update( { chromosome + "_%_" + str( start+1 ): None } )
+						css_splice_site_pos.update( { chromosome + "_%_" + str( start+2 ): None } )
+				elif parts[1:3] == [ "GC", "AG" ] or parts[1:3] == [ "AT", "AC" ]:
+					major_ncss_total_len += 4
+					for each in subparts:
+						start = int( each.split('_')[0] )
+						major_ncss_splice_site_pos.update( { chromosome + "_%_" + str( start+1 ): None } )
+						major_ncss_splice_site_pos.update( { chromosome + "_%_" + str( start+2 ): None } )
+				else:
+					minor_ncss_total_len += 4
+					for each in subparts:
+						start = int( each.split('_')[0] )
+						minor_ncss_splice_site_pos.update( { chromosome + "_%_" + str( start+1 ): None } )
+						minor_ncss_splice_site_pos.update( { chromosome + "_%_" + str( start+2 ): None } )
 			line = f.readline()
-	return minor_ncss, total_splice_site_length
+	return css_splice_site_pos, major_ncss_splice_site_pos, minor_ncss_splice_site_pos, css_total_len, major_ncss_total_len, minor_ncss_total_len
 
 
-def load_variation_rate_from_vcf( vcf_file ):
-	"""! @brief load variation rate from given VCF file
-	@warning only homozygous variants against the reference are considered
-	"""
+def load_variants( vcf_file ):
+	"""! @brief load all variants from given VCF """
 	
-	# --- generate all keys --- #
-	variation_counts = {}
-	for nt1 in [ "A", "C", "G", "T" ]:
-		for nt2 in [ "A", "C", "G", "T" ]:
-			if nt1 != nt2:
-				variation_counts.update( { nt1+nt2: 0 } )
+	variants = {}
 	
-	# --- process file --- #
 	with open( vcf_file, "r" ) as f:
 		line = f.readline()
 		while line:
 			if line[0] != '#':
 				parts = line.strip().split('\t')
-				if len( parts[3] ) == 1:
-					if len( parts[4] ) == 1:
-						if parts[3] != parts[4]:
-							if parts[3] != "N" and parts[4] != "N":
-								variation_counts[ parts[3]+parts[4] ] += 1
+				variants.update( { parts[0] + "_%_" + parts[1]: None } )
 			line = f.readline()
-	
-	return variation_counts
-
-
-def get_variants( query, ref ):
-	"""! @brief get variants from ref to query """
-	
-	variants = []
-	for idx, nt in enumerate( query ):
-		if nt != ref[ idx ]:
-			variants.append( ref[ idx ]+nt )
 	return variants
 
 
-def calculate_variation_rate_for_ncss( minor_ncss ):
-	"""! @brief calcualte variation ratios for minor ncss """
+def get_genome_size( fasta_file ):
+	"""! @brief get genome size """
 	
-	# --- generate all keys --- #
-	variation_counts = {}
-	for nt1 in [ "A", "C", "G", "T" ]:
-		for nt2 in [ "A", "C", "G", "T" ]:
-			if nt1 != nt2:
-				variation_counts.update( { nt1+nt2: 0 } )
+	genome_size = 0
+	with open( fasta_file, "r" ) as f:
+		line = f.readline()
+		while line:
+			if line[0] != '>':
+				genome_size += len( line.strip() )
+			line = f.readline()
+	return genome_size
+
+
+def get_matches( variants, splice_site_pos, total_len ):
+	"""! @brief calcualte matches """
 	
-	# --- investigate data --- #
-	for ncss in minor_ncss:
-		gtag = get_variants( ncss, "GTAG" )
-		gcag = get_variants( ncss, "GCAG" )
-		atac = get_variants( ncss, "ATAC" )
-		
-		if gtag >= gcag and gtag >= atac:
-			for each in gtag:
-				if not "N" in each:
-					variation_counts[ each ] += 1
-		elif gcag >= gtag and gcag >= atac:
-			for each in gcag:
-				if not "N" in each:
-					variation_counts[ each ] += 1
-		else:
-			for each in atac:
-				if not "N" in each:
-					variation_counts[ each ] += 1
-	
-	return variation_counts
+	match = 0
+	mismatch = 0
+	for variant in variants.keys():
+		try:
+			splice_site_pos[ variant ]
+			match += 1
+		except KeyError:
+			mismatch += 1
+	return ( 100.0 * match ) / total_len
 
 
 def main( arguments ):
@@ -111,27 +124,27 @@ def main( arguments ):
 	
 	vcf_file = arguments[ arguments.index('--vcf')+1 ]		#VCF file containing variants of the same species
 	ncss_file = arguments[ arguments.index('--ncss')+1 ]	#splice sites file
+	gff_file = arguments[ arguments.index('--gff')+1 ]	#GFF3 file
+	fasta_file = arguments[ arguments.index('--fasta')+1 ]	#FASTA file of genome
 	
-	minor_ncss, total_splice_site_length = load_ncss( ncss_file )
-	variation_rate = load_variation_rate_from_vcf( vcf_file )
-	minor_ncss_variation_rate = calculate_variation_rate_for_ncss( minor_ncss )
 	
-	ncss_values = []
-	global_values = []
-	for key in sorted( minor_ncss_variation_rate.keys() ):
-		ncss_values.append( minor_ncss_variation_rate[ key ] )
-		global_values.append( variation_rate[ key ] )
+	# --- check the mutational load of splice sites --- #
+	chr_per_gene = load_chr_per_gene( gff_file )
+	css_splice_site_pos, major_ncss_splice_site_pos, minor_ncss_splice_site_pos, css_total_len, major_ncss_total_len, minor_ncss_total_len = load_splice_site_pos( ncss_file, chr_per_gene )
+	variants = load_variants( vcf_file )
+	genome_size = get_genome_size( fasta_file )
 	
-	print sorted( minor_ncss_variation_rate.keys() )
-	print ncss_values
-	print global_values
-	
-	print "comparison of splice site diversity to overall variation pattern:"
-	print stats.chisquare( ncss_values,  f_exp=global_values )
+	print len( css_splice_site_pos.keys() )
+	print len( major_ncss_splice_site_pos.keys() )
+	print len( minor_ncss_splice_site_pos.keys() )
+		
+	print "percentage of canonical splice site positions with variants: " + str( get_matches( variants, css_splice_site_pos, css_total_len ) ) + "%"
+	print "percentage of major ncss positions with variants: " + str( get_matches( variants, major_ncss_splice_site_pos, major_ncss_total_len ) ) + "%"
+	print "percentage of minor ncss positions with variants: " + str( get_matches( variants, minor_ncss_splice_site_pos, minor_ncss_total_len ) ) + "%"
 
 
 if __name__ == '__main__':
-	if '--vcf' in sys.argv and '--ncss' in sys.argv:
+	if '--vcf' in sys.argv and '--ncss' in sys.argv and '--gff' in sys.argv and '--fasta' in sys.argv:
 		main( sys.argv )
 	else:
 		sys.exit( __usage__ )
